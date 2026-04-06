9887f2b26c8da691c3c5d70416332639f88dc614
poojasahu021994 <poojasahu02071994@gmail.com>
2026-04-06 16:56:33 +0530

    update

--- smart-reminder-frontend/src/pages/Login.jsx
+++ smart-reminder-frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const handleLogin = async () =>{
 try{
 
 const res = await axios.post(
-"https://remainderssystem.onrender.com/api/login/",
+"hhttps://remainderssystem.onrender.com/api/login/",
 {username,password}
 )
 
